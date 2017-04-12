5b0f4c9bf39c7c306af8fea81258306857ee066e
federico.aloi@gmail.com <federico.aloi@gmail.com>
2017-04-12 10:22:29 -0400

    Mumuki Export on 2017-04-12 10:22:29 -0400

--- 00007_Conociendo el país/test..rb
+++ 00007_Conociendo el país/test..rb
@@ -1,5 +1,21 @@
+module Pepita
+  def self.ciudad
+    @ciudad
+  end
+end
+
 describe '' do
   it 'Iruya existe' do
     expect(Iruya).to be
+  end
+  
+  it 'Obera existe' do
+    expect(Obera).to be
+  end
+  
+  context 'Pepita' do
+    it 'empieza en Iruya' do
+      expect(Pepita.ciudad).to be Iruya
+    end
   end
 end
