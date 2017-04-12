7ba3fd0cb93e3b0088b7238495ccadf44cce1db4
federico.aloi@gmail.com <federico.aloi@gmail.com>
2017-04-12 10:36:46 -0400

    Mumuki Export on 2017-04-12 10:36:46 -0400

--- 00007_Conociendo el país/test..rb
+++ 00007_Conociendo el país/test..rb
@@ -18,6 +18,10 @@ describe '' do
   end
   
   context 'Pepita' do
+    it 'empieza con 100 de energía' do
+      expect(Pepita.energia).to eq 100
+    end
+    
     it 'empieza en Iruya' do
       expect(Pepita.ciudad).to eq Iruya
     end
@@ -28,7 +32,7 @@ describe '' do
     
     it 'pierde 100 unidades de energía cuando vuela' do
       Pepita.volar_hacia!(Obera)
-      expect(Pepita.energia).to eq 90
+      expect(Pepita.energia).to eq 0
     end    
     
     it 'cambia de ciudad cuando vuela' do
