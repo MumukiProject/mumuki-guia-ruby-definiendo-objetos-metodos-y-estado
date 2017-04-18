03245fd865bd8988547558aaae3c9037b16cff03
federico.aloi@gmail.com <federico.aloi@gmail.com>
2017-04-18 19:01:17 -0400

    Mumuki Export on 2017-04-18 19:01:17 -0400

---
+++ 00009_Nuevo ejercicio/extra.rb
@@ -0,0 +1,26 @@
+/*[IgnoreContentOnQuery]*/
+
+module Pepita
+  @energia = 100
+  @ciudad = Obera
+  
+  #...más cosas que ahora no interesan...
+end
+
+module Obera
+  #...más cosas que ahora no interesan...
+end
+
+module Kiano1100
+  #...más cosas que ahora no interesan...
+end
+
+module RolamotoC115
+  #...más cosas que ahora no interesan...
+end
+
+module Enrique
+  @celular = Nokia1100
+  @dinero_en_billetera = 13
+  @frase_favorita = 'la juventud está perdida'
+end
